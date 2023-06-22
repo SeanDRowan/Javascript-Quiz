c18c3cc2acc0089f0fdfac4b76ec53675b7972b2
Sean <sdrowan93@gmail.com>
2023-06-22 13:29:08 -0500

    intial setup

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const startButton = document.getElementById('start');
+
+const Questions = [
+    {
+      question: "question 1",
+      answers: {
+        a: "1",
+        b: "2",
+        c: "3"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "question 2",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "c"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "question 3",
+      answers: {
+        a: "1r",
+        b: "j2",
+        c: "R3",
+        d: "4"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+function buildQuiz(){}
+
+function showResults(){}
