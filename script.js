90e5b4d3b8ef98a1f1891f1c2edf8deff2179aa5
Sean <sdrowan93@gmail.com>
2023-06-23 16:13:06 -0500

    finished html, questions populate from questions array

--- script.js
+++ script.js
@@ -1,38 +1,64 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const startButton = document.getElementById('start');
 
-const Questions = [
-    {
-      question: "question 1",
-      answers: {
-        a: "1",
-        b: "2",
-        c: "3"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "question 2",
-      answers: {
-        a: "a",
-        b: "b",
-        c: "c"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "question 3",
-      answers: {
-        a: "1r",
-        b: "j2",
-        c: "R3",
-        d: "4"
-      },
-      correctAnswer: "d"
-    }
-  ];
+const start= document.getElementById('start')
+const questionContainerE =document.getElementById('question-container')
+const questionE = document.getElementById('question')
+const answerButtonsE = document.getElementById('answer-buttons')
+const questions = [
+  {
+    question:'question1',
+    answers:[
+      {text:'answer choice1',correct:true},
+      {text:'answer choice2',correct:false},
+      {text:'answer choice3',correct:false},
+      {text:'answer choice4',correct:false}
+    ]
+  }
+]
 
-function buildQuiz(){}
+let question1 ,currentquestionIndex
 
-function showResults(){}
+function startQuiz(){
+    start.classList.add('hide')
+    questionContainerE.classList.remove('hide')
+    currentquestionIndex=0
+    nextQuestion();
+}
+
+    
+function scoreBoard(){}
+
+function countDown(){
+    var timeLeft = 20;
+    var timeInterval = setInterval(function () {
+     
+     if ( timeLeft > 1 ){ 
+      timerEl.textContent = timeLeft+ 'seconds'
+      timeLeft--;
+     }
+      else if ( timeLeft == 1 ){
+        timerEl.textContent=("you have" + timeLeft +" second")
+        timeLeft--;
+      }
+        else{
+          timerEl.textContent =""
+          clearInterval(timeInterval);
+          displayMessage();
+        }
+    },1000);
+}
+
+
+function nextQuestion(question){
+  showquestion(questions[currentquestionIndex])
+
+}
+
+function showquestion(question){
+questionE.innerText= question.question
+}
+
+function reset(){}
+
+
+start.addEventListener( "click", startQuiz)
+
